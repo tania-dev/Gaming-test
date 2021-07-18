20b7d7f84fdaa3d69855f4ed7c8baf587375a702
Muhammad Lahin <muhammad.lahin@gmail.com>
2021-07-18 21:16:42 +0600

    Gaming page updated

--- client/src/components/navigations/Navbar.jsx
+++ client/src/components/navigations/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, Link } from "react-router-dom";
 import Button from '@material-ui/core/Button';
@@ -9,12 +9,17 @@ function Navbar() {
     const dispatch = useDispatch();
     const history = useHistory();
     const state = useSelector(state => state);
+    const [user, setUser] = useState({});
 
     useEffect(() => {
         if(!state.login) {
             history.push("/login")
         }
-    }, [state, history])
+        setTimeout(function() {
+            let user = JSON.parse(localStorage.getItem('user'))
+            setUser(user)
+        }, 50);
+    }, [user, state, history])
 
     const handleLogout = () => {
         dispatch(makeLogout());
@@ -23,9 +28,15 @@ function Navbar() {
     return (
         <nav className="navbar-section">
             <div className="navbar-items">
+                {user && Object.keys(user).length > 0 ? <>
                 <Link to="/">Home</Link>
                 <Link to="/game">Game</Link>
                 <Button onClick={handleLogout}>Logout</Button>
+                
+                </> : <>
+                <Link to="/login">Login</Link>
+                <Link to="/registration">Registration</Link>                
+</>}
             </div>
         </nav>
     )
--- client/src/pages/Game/Game.jsx
+++ client/src/pages/Game/Game.jsx
@@ -4,11 +4,6 @@ import Button from '@material-ui/core/Button';
 import './Game.scss'
 import { gameStart } from '../../redux/actions';
 
-const formatter = new Intl.ListFormat('en', {
-    style: 'long',
-    type: 'conjunction'
-})
-
 function Game() {
     const [totalCoins, setTotalCoins] = useState(20);
     const dispatch = useDispatch();
@@ -21,7 +16,6 @@ function Game() {
     const handleRoll = () => {
         dispatch(gameStart(totalCoins - 1))
     }
-
     return (
         <div className="game-section">
             <div className="game">
@@ -29,8 +23,16 @@ function Game() {
                     <Button variant="contained" color="secondary" onClick={handleRoll}>Roll</Button>
                 </div>
                 <div className="items-section">
-                    <p>{formatter.format(state.game.items)}</p>
-                    <h3>Total Coins: {state.game.coins}</h3>
+                    <div className="items">
+                        {state.game.items.map((item, index) => {
+                            return (
+                                <p key={index} className="item">{item}</p>
+                            )
+                        })}
+                    </div>
+                    <div className="coins-section">
+                        <h3>Total Coins: {state.game.coins}</h3>
+                    </div>
                 </div>
             </div>
         </div>
